6e7a2e633efc38d4b9b5aa29d8be0066ca085cee
nodefried-and-roy <alfred@joestrusz.com>
2018-08-26 17:47:36 -0500

    Testing...

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-// Misc Basic Variables
+//
+// START SECTION: SYSTEM
+//
+
+// START: Constants
 const config = require('./config.json');
 const os = require('os');
 const fs = require('fs');
@@ -8,7 +12,9 @@ const path = require('path');
 const express = require('express');
 const request = require("request");
 const http = require("http");
+// END: Constants
 
+// START: Other Variables
 var sys = require('util');
 var exec = require('child_process').exec;
 var cluster = require('cluster');
@@ -16,26 +22,54 @@ var systemOS = os.platform();
 var prettySize = require('prettysize');
 var prettyMs = require('pretty-ms');
 var ffmpeg = require('fluent-ffmpeg');
+// END: Other Variables
 
 
-// Colors
+// START: Config Value Variables
+bot_nickname = config.bot_nickname;
+bot_web_port = config.bot_web_port;
+// END: Config Value Variables
+
+// START: Color Variables
 var colors = require('colors');
+// END: Color Variables
 
-// Timestamp Log
+//
+// END SECTION: SYSTEM
+//
+
+//
+// START SECTION: FUNCTIONS
+//
+
+// START: Write Operator Data
+function operatorSave() {
+	fs.writeFile("test", "Hey there!", function(err) {
+		if(err) {
+	    		return console.log(err);
+		}
+		console.log("The file was saved!");
+	});
+}
+// END: Write Operator Data
+
+// START: Timestamp Log
 function timeStampLog() {
 	var dateTime = require('node-datetime');
 	var dt = dateTime.create();
 	return dt.format('Y-m-d H:M:S').bold.green+'| ';
 }
+// END: Timestamp Log
 
-// Timestamp Normal
+// START: Timestamp Normal
 function timeStamp() {
 	var dateTime = require('node-datetime');
 	var dt = dateTime.create();
 	return dt.format('Y-m-d H:M:S');
 }
+// END: Timestamp Normal
 
-// Ping
+// START: Ping
 function ping(host) {
 	var sys = require('util');
 	var exec = require('child_process').exec;
@@ -50,8 +84,9 @@ function ping(host) {
 		exec("ping -c 5 "+host, puts);
 	}
 };
+// END: Ping
 
-// Prompt
+// START: Prompt
 function prompt(question, callback) {
 	var stdin = process.stdin,
 	stdout = process.stdout;
@@ -63,13 +98,15 @@ function prompt(question, callback) {
 		callback(data.toString().trim());
 	});
 }
+// END: Prompt
 
-// Console Prompt
+// START: Console Prompt
 function botConsolePrompt() {
 	return bot_nickname.toLowerCase().yellow+'@localhost'.yellow+' ##_\ '.trap.bold.cyan;
 }
+// END: Console Prompt
 
-// Console
+// START: Console
 function botConsole() {
 	prompt(timeStampLog()+botConsolePrompt(), function(botCommand) {
 		var arguments = botCommand.split(/(\s+)/);
@@ -97,8 +134,9 @@ function botConsole() {
 		}
 	})
 }
+// END: Console
 
-// Web Server
+// START: Web Server
 function webServer(action) {
 	const web = express();
 	if (action.toUpperCase() == "START") {
@@ -122,13 +160,25 @@ function webServer(action) {
 		})
 	}
 }
+// END: Web Server
 
-// Start Runtime
+
+//
+// END SECTION: FUNCTIONS
+//
+
+
+//
+// START SECTION: RUNTIME
+//
+
+// START: Initial Prompt and Console
 prompt(timeStampLog()+'Please enter your name so I know what to call you? ', function (var_operator_name) {
     prompt(timeStampLog()+'Hello, '+var_operator_name+' I am '+bot_nickname+'. Do you want to hear a joke? ', function (var_question_result) {
 	if (var_question_result.toUpperCase() == "YES" || var_question_result.toUpperCase() == "OK" || var_question_result.toUpperCase() == "SURE" ) {
 		prompt(timeStampLog()+'OH! What do you call a bot with no legs '+var_operator_name+'? ', function (var_joke_answer) {
 			console.log(timeStampLog()+"A Python bot! Haha! Get it? Because Python's don't have legs.");		
+			operatorSave();
 			botConsole();
 		})
 	} else {
@@ -136,4 +186,8 @@ prompt(timeStampLog()+'Please enter your name so I know what to call you? ', fun
 	}
     })
 });
-// End Runtime
+// END: Initial Prompt and Console
+
+//
+// END SECTION: RUNTIME
+//
